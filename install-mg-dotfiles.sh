34d7d76915a1ac51b512a9481400513a5f2be358
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-02-06 21:08:51 +0100

    Remove shallow clone from install-mg-dotfiles as history is important

--- install-mg-dotfiles.sh
+++ install-mg-dotfiles.sh
@@ -2,7 +2,7 @@
 (cd ~ && \
 	git init --quiet && \
 	grep -q '\[remote "origin"\]' .git/config  || git remote add origin https://github.com/mgrzaslewicz/dotfiles.git && \
-	git fetch --depth=1 origin && \
+	git fetch origin && \
 	git checkout -f master \
 	git pull --rebase \
 )
