110587a1d098ba899ba1a652e4f63c286294f653
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-02-06 09:56:01 +0100

    Add $REPOS and $MY_TMP

--- .aliases-mg/navigation.zsh
+++ .aliases-mg/navigation.zsh
@@ -1,5 +1,10 @@
 # navigation
-alias repos="cd ~/repos"
+export REPOS=~/repos
+export MY_TMP=~/tmp
+mkdir -p $REPOS
+mkdir -p $MY_TMP
+alias repos="cd $REPOS"
+alias mytmp="cd $MY_TMP"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
