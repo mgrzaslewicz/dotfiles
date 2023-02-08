9009e6d5d6f9e984dc3681f38a657c296d38aab3
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-02-08 09:37:05 +0100

    Fix clone script

--- install-mg-dotfiles.sh
+++ install-mg-dotfiles.sh
@@ -3,7 +3,7 @@
 	git init --quiet && \
 	grep -q '\[remote "origin"\]' .git/config  || git remote add origin https://github.com/mgrzaslewicz/dotfiles.git && \
 	git fetch origin && \
-	git checkout -f master \
+	git checkout -f master && \
 	git pull --rebase \
 )
 (cd ~/.aliases-mg && ./install-zsh-aliases.sh)
