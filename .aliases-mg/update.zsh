3b5970c77de8d5eda944dd47a0604e51b1baa39d
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-04-05 11:46:45 +0200

    Add apt autoremove to system upgrade

--- .aliases-mg/update.zsh
+++ .aliases-mg/update.zsh
@@ -1,2 +1,3 @@
 alias upall="sudo apt update && sudo apt -y full-upgrade && sudo apt autoremove -y && sudo apt autoclean && sudo snap refresh"
 alias upallpip="pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
+alias provision="(cd ~/repos/dev-machine-provision && ./provision.sh)"
