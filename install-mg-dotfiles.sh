355258618ed6c3c4cdc8085e7db680e0811e9fab
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-06-29 19:20:01 +0200

    Add global git user config

--- install-mg-dotfiles.sh
+++ install-mg-dotfiles.sh
@@ -14,7 +14,7 @@ git checkout master
 git pull --rebase
 git config --global include.path ~/gitalias.txt
 git config --global include.path ~/my-gitalias.txt
-git config --global user.email mikolaj.grzaslewicz@gmail.com
-git config --global user.name Mikolaj Grzaslewicz
+git config --global user.email "mikolaj.grzaslewicz@gmail.com"
+git config --global user.name "Mikolaj Grzaslewicz"
 
 (cd ~/.aliases-mg && ./install-zsh-aliases.sh)
