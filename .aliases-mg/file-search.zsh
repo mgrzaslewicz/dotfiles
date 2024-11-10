b37bdedd468cad6d3341d58c9554ed2cc51bbc99
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2024-11-10 12:54:34 +0100

    Fix greperrors alias, it was including only occurences of words missing entries like 'SomeException'

--- .aliases-mg/file-search.zsh
+++ .aliases-mg/file-search.zsh
@@ -1,3 +1,3 @@
 # boost file search
-alias greperrors="grep -i -w 'fatal\|error\|exception\|timeout\|waiting\|fail\|unable\|lock\|block'"
+alias greperrors="grep -i 'fatal\|error\|exception\|timeout\|waiting\|fail\|unable\|lock\|block'"
 alias grepanywhere="grep -nr . -e $1"
