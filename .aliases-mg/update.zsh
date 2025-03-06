26e4052c0d1c523034bf0eaf03d858645150c445
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2025-03-06 19:03:23 +0100

    Fix upall alias, it was only doing apt update

--- .aliases-mg/update.zsh
+++ .aliases-mg/update.zsh
@@ -1,3 +1,3 @@
-alias upall="sudo apt update && sudo apt -y full-upgrade && sudo apt autoremove -y && sudo apt autoclean && sudo snap refresh"
+alias upall="sudo apt update; sudo apt upgrade; sudo apt -y full-upgrade; sudo apt autoremove -y; sudo apt autoclean; sudo snap refresh"
 alias upallpip="pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
 #alias provision="(cd ~/repos/workstation-provision && ./provision.sh)"
