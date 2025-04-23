bce04c20f5cf9d00c538e6c580203a81a6fd1a5b
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2025-04-23 16:17:30 +0200

    Make the project consumable by https://github.com/twpayne/chezmoi

--- run_on_change_git_config.sh
+++ run_on_change_git_config.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-
-REPO=https://github.com/mgrzaslewicz/dotfiles.git
-cd ~
-git init --quiet
-if ! grep -q '\[remote "origin"\]' .git/config; then
-  git remote add origin $REPO
-fi
-git config --unset core.bare
-git fetch origin
-git checkout master
-git pull --rebase
-
 # when the list is empty, it will return non-zero code so prevent it
 git config --global --unset-all include.path || true
 git config --global --add include.path ~/gitalias.txt
@@ -20,5 +8,3 @@ git config --global --add include.path ~/my-gitalias.txt
 
 git config --global user.email "mikolaj.grzaslewicz@gmail.com"
 git config --global user.name "Mikolaj Grzaslewicz"
-
-(cd ~/.aliases-mg && ./install-zsh-aliases.sh)
