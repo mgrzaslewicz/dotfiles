30a1e8d4d273d77244b102909f9b76fb4508314c
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2025-10-06 10:33:53 +0200

    Make 'intellij' alias work both with 'intellij-idea-ultimate' executable and fallback to 'idea'
    So now it works also on systems with different intellij executable name

--- dot_oh-my-zsh/custom/executable_intellij.zsh
+++ dot_oh-my-zsh/custom/executable_intellij.zsh
@@ -1,2 +1,2 @@
-alias intellij="intellij-idea-ultimate ."
-alias intellij-rmlock="rm ~/.config/JetBrains/IntelliJIdea*/.lock"
+alias intellij='command -v intellij-idea-ultimate >/dev/null 2>&1 && intellij-idea-ultimate . || idea .'
+alias intellij-rmlock='rm ~/.config/JetBrains/IntelliJIdea*/.lock'
