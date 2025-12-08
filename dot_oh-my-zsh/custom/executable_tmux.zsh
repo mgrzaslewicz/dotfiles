fb8d9c38bba8e6386f16b6e85e9ef4bcfd9a3b2a
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2025-12-08 15:10:18 +0100

    Start or attach to a tmux session on zsh start

--- dot_oh-my-zsh/custom/executable_tmux.zsh
+++ dot_oh-my-zsh/custom/executable_tmux.zsh
@@ -1,2 +1,5 @@
-# tmux
+# tmux helpers and optional auto-start
 alias tmux-print-screen="tmux capture-pane -pS -1000000"
+if [[ -z "$TMUX" ]]; then
+  tmux attach-session -t "$USER" || tmux new-session -s "$USER"
+fi
