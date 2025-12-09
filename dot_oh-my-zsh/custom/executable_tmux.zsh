f3dd2c6f2e82a346f702201c9e0fa821fc72b7c7
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2025-12-09 10:20:59 +0100

    Handle starting/attaching to tmux session with given working dir

--- dot_oh-my-zsh/custom/executable_tmux.zsh
+++ dot_oh-my-zsh/custom/executable_tmux.zsh
@@ -1,5 +1,10 @@
 # tmux helpers and optional auto-start
 alias tmux-print-screen="tmux capture-pane -pS -1000000"
 if [[ -z "$TMUX" ]]; then
-  tmux attach-session -t "$USER" || tmux new-session -s "$USER"
+  if tmux has-session -t "$USER" 2>/dev/null; then
+    tmux new-window -t "$USER" -c "$PWD"
+    tmux attach-session -t "$USER"
+  else
+    tmux new-session -s "$USER" -c "$PWD"
+  fi
 fi
