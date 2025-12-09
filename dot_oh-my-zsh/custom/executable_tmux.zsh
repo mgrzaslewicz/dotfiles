27e437297f03cbaf77266867a22fabf4fb7f3eaa
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2025-12-09 11:07:41 +0100

    Don't sanitize tmux session name, it's not needed

--- dot_oh-my-zsh/custom/executable_tmux.zsh
+++ dot_oh-my-zsh/custom/executable_tmux.zsh
@@ -1,13 +1,7 @@
 # tmux helpers and optional auto-start
 alias tmux-print-screen="tmux capture-pane -pS -1000000"
 if [[ -z "$TMUX" ]]; then
-  # Sanitize working directory for tmux session name (replace / with -)
-  SESSION_NAME="${PWD//\//-}"
-  # Remove leading - if path starts with /
-  SESSION_NAME="${SESSION_NAME#-}"
-  # Use root as session name if we're at /
-  [[ -z "$SESSION_NAME" ]] && SESSION_NAME="root"
-
+  SESSION_NAME="${PWD}"
   if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
     tmux new-window -t "$SESSION_NAME" -c "$PWD"
     tmux attach-session -t "$SESSION_NAME"
