34f305446fc1746bf718df822c977e58284d6799
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2026-05-26 08:18:58 +0000

    Use lsd aliases only when lsd command is available

--- dot_oh-my-zsh/custom/executable_lsd.zsh
+++ dot_oh-my-zsh/custom/executable_lsd.zsh
@@ -1,3 +1,5 @@
-alias ls="lsd"
-alias ll="lsd -lah"
-alias llf="lsd -lah | grep $1"
+if command -v lsd >/dev/null 2>&1; then
+    alias ls="lsd"
+    alias ll="lsd -lah"
+    alias llf="lsd -lah | grep $1"
+fi
