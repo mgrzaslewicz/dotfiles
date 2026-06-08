48ecab08691ef852aff0c32d1b0e34d3841bb4c8
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2026-06-08 18:52:26 +0000

    Add vv command for recursive current dir list

--- dot_oh-my-zsh/custom/executable_v.zsh
+++ dot_oh-my-zsh/custom/executable_v.zsh
@@ -1,6 +1,24 @@
+# Non recursive current dir list
 v() {
   local selected
-  selected=$(find * . -maxdepth 8 | fzf)
+  # -mindepth 1 eliminates '.'
+  selected=$(find . -maxdepth 1 -mindepth 1 | fzf)
+
+  if [[ -n "$selected" ]]; then
+    if [[ -d "$selected" ]]; then
+      cd "$selected" || exit
+    elif [[ $(file --mime-type -b "$selected") == text/* ]]; then
+      vi "$selected"
+    else
+      ls -l "$selected"
+    fi
+  fi
+}
+
+# Recursive current dir list
+vv() {
+  local selected
+  selected=$(find . -maxdepth 8 -mindepth 1 | fzf)
 
   if [[ -n "$selected" ]]; then
     if [[ -d "$selected" ]]; then
