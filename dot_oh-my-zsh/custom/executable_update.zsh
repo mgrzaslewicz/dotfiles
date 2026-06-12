11b6e690492cd2e4f527ae061b6b5693915c8e22
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2026-06-12 23:23:36 +0200

    Make 'upall' alias work also on macos and robust to non existing packaging commands

--- dot_oh-my-zsh/custom/executable_update.zsh
+++ dot_oh-my-zsh/custom/executable_update.zsh
@@ -1,2 +1,24 @@
-alias upall="sudo apt update; sudo apt upgrade -y; sudo apt -y full-upgrade; sudo apt autoremove -y; sudo apt autoclean; sudo snap refresh; flatpak update -y"
+upall() {
+  if command -v apt &> /dev/null; then
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt -y full-upgrade
+    sudo apt autoremove -y
+    sudo apt autoclean
+  fi
+
+  if command -v snap &> /dev/null; then
+    sudo snap refresh
+  fi
+
+  if command -v flatpak &> /dev/null; then
+    flatpak update -y
+  fi
+
+  if command -v brew &> /dev/null; then
+    brew update
+    brew upgrade
+  fi
+}
+
 alias upallpip="pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
