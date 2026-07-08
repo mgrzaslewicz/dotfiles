21049bb12c586d8ad9bbcea490d8985cbf4c5f97
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2026-07-08 11:05:46 +0200

    Rebase before push in gbackup alias so pushing will be less ilkely to fail

--- dot_oh-my-zsh/custom/executable_git.zsh
+++ dot_oh-my-zsh/custom/executable_git.zsh
@@ -2,4 +2,4 @@
 alias g="git"
 alias prpu="git pull --rebase && git push"
 alias ggrepall="git branch -a | tr -d \* | sed '/->/d' | xargs git grep"
-alias gbackup="git add * && g commit -m 'Manual backup' && git push"
+alias gbackup="git add * && g commit -m 'Manual backup' && git pull --rebase && git push"
