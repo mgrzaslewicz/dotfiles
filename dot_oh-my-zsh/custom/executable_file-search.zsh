69061951dd5b51ad7fea65b08918fe9d31352c72
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2026-07-28 15:27:41 +0000

    Add rgf: fuzzy ripgrep-to-editor content search
    Only defined when rg and fzf are both installed. Filters live rg hits
    through fzf with in-context preview, then opens the pick at the matched line.

--- dot_oh-my-zsh/custom/executable_file-search.zsh
+++ dot_oh-my-zsh/custom/executable_file-search.zsh
@@ -1,3 +1,20 @@
 # boost file search
 alias greperrors="grep -i 'fatal\|error\|exception\|timeout\|waiting\|fail\|unable\|lock\|block'"
 alias grepanywhere="grep -nr . -e $1"
+
+if command -v rg >/dev/null 2>&1 && command -v fzf >/dev/null 2>&1; then
+  # fuzzy-filter ripgrep hits, preview in context, open at the matched line
+  rgf() {
+    local preview_cmd='cat {1}'
+    command -v bat >/dev/null 2>&1 && preview_cmd='bat --style=numbers --color=always --highlight-line {2} {1}'
+
+    local picked file line
+    picked=$(rg --line-number --no-heading . |
+      fzf --delimiter : --preview "$preview_cmd" --preview-window '+{2}-/2')
+
+    [[ -z "$picked" ]] && return
+    file=$(cut -d: -f1 <<<"$picked")
+    line=$(cut -d: -f2 <<<"$picked")
+    vi "$file" "+$line"
+  }
+fi
